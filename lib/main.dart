db065a7326c94a62304f05c77ab7253def12a5de
Raju Mondal <rajumondalr82@gmail.com>
2023-01-29 17:29:59 +0600

    reactive way of getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx/mycontroller.dart';
 
 void main() {
   runApp(
@@ -18,21 +19,34 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var text = "click".obs;
-  RxInt count = 0.obs;
+  // var text = "click".obs;
+  // RxInt count = 0.obs;
+  // List text = ['item1', 'item2'].obs; //import this list from MyController
+  MyController myController = Get.put(MyController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home Page"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Obx(() => Text('${text.value} ${count.value}')),
-          onPressed: () {
-            text.value = 'clicked';
-            count.value++;
-          },
+      // body: Center(
+      //   child: ElevatedButton(
+      //     child: Obx(() => Text('${text.value} ${count.value}')),
+      //     onPressed: () {
+      //       text.value = 'clicked';
+      //       count.value++;
+      //     },
+      //   ),
+      // ),
+      body: Obx(
+        () => ListView.builder(
+          itemCount: myController.text.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(myController.text[index]),
+            onTap: () {
+              myController.text.add('item $index');
+            },
+          ),
         ),
       ),
     );
---
+++ lib/mycontroller.dart
@@ -0,0 +1,5 @@
+import 'package:get/get.dart';
+
+class MyController extends GetxController {
+  List text = ['item 1', 'item 2'].obs;
+}
